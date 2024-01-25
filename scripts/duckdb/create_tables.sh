acc97314bb735a5f3699c9c0812b564ab6df5935
Geoff Cox <redgeoff@gmail.com>
2024-01-25 08:43:05 -0800

    pg schema (#6)

--- scripts/duckdb/create_tables.sh
+++ scripts/duckdb/create_tables.sh
@@ -6,4 +6,4 @@ source .env
 
 duckdb ${DUCKDB_FILE} -c "INSTALL postgres"
 
-duckdb ${DUCKDB_FILE} -c "LOAD postgres; CALL postgres_attach('${PG_CONNECTION_STRING}');"
+duckdb ${DUCKDB_FILE} -c "LOAD postgres; CALL postgres_attach('${PG_CONNECTION_STRING}', source_schema='${PG_SCHEMA}');"
