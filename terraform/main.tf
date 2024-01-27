c49779debccc8bc3f83eba871374a95b91819635
Geoff Cox <redgeoff@gmail.com>
2024-01-27 08:59:59 -0800

    fix index

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   filename    = "${path.module}/../ignored/log-exporter.zip"
   function_name = "Log_Exporter_Function"
   role        = aws_iam_role.lambda_role.arn
-  handler     = "index.lambda_handler"
+  handler     = "lambda_handler.lambda_handler"
   runtime     = "python3.8"
   depends_on  = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
