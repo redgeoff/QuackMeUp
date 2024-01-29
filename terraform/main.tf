1f1e840c474ef073baffc6c245c4f5b7f5951579
Geoff Cox <redgeoff@gmail.com>
2024-01-29 10:33:32 -0800

    lambda with docker image

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,98 +88,41 @@ data "aws_iam_policy_document" "ecr_policy" {
   }
 }
 
-# resource "aws_iam_role_policy" "ecr_policy" {
-#   name   = "ecr_policy"
-#   role   = aws_iam_role.lambda_role.id
-#   policy = data.aws_iam_policy_document.ecr_policy.json
-# }
+resource "aws_iam_role" "lambda_role" {
+  name = "log_exporter_function_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role" "lambda_role" {
-#   name = "log_exporter_function_role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_lambda_function" "log_exporter_function" {
+  function_name = "log_exporter_function"
 
-# resource "aws_iam_role" "lambda_role" {
-#   name = "log_exporter_function_role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+  # The Docker image URI
+  image_uri = "${aws_ecr_repository.quackmeup_repository.repository_url}:latest"
 
-# resource "aws_iam_policy" "iam_policy_for_lambda" {
-#   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
-#   path        = "/"
-#   description = "AWS IAM Policy for managing aws lambda role"
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": [
-#         "logs:CreateLogGroup",
-#         "logs:CreateLogStream",
-#         "logs:PutLogEvents"
-#       ],
-#       "Resource": "arn:aws:logs:*:*:*",
-#       "Effect": "Allow"
-#     }
-#   ]
-# }
-# EOF
-# }
+  # The command that is passed to the function
+  package_type = "Image"
+  timeout      = 60
 
-# resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-#   role       = aws_iam_role.lambda_role.name
-#   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
-# }
+  # IAM role
+  role = aws_iam_role.lambda_role.arn
 
-# resource "null_resource" "build_and_package_lambda" {
-#   provisioner "local-exec" {
-#     command = <<EOF
-#       cd ..
-#       docker build -t lambda-builder .
-#       docker run --name lambda-builder-container lambda-builder
-#       docker cp lambda-builder-container:/var/task ./ignored/lambda_package
-#       docker rm lambda-builder-container
-#       cd ./ignored/lambda_package && zip -r ../lambda_package.zip .
-#     EOF
-#   }
-#   triggers = {
-#     build_trigger = "${timestamp()}"
-#   }
-# }
-
-# resource "aws_lambda_function" "terraform_lambda_func" {
-#   filename    = "lambda_package.zip"
-#   function_name = "log_exporter_function"
-#   role        = aws_iam_role.lambda_role.arn
-#   handler     = "lambda_handler.lambda_handler"
-#   runtime     = "python3.11"
-#   depends_on  = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, resource.null_resource.build_and_package_lambda]
-# }
+  # VPC settings, if necessary
+  # vpc_config {
+  #   subnet_ids         = [aws_subnet.log_exporter_function.id]
+  #   security_group_ids = [aws_security_group.log_exporter_function.id]
+  # }
+}
