0d830d6e87b5e21232f2245fd7f0a1889a122f95
Geoff Cox <redgeoff@gmail.com>
2024-02-05 13:46:50 -0800

    force destroy comments

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "quackmeup_bucket" {
   bucket = var.logs_bucket_name
 
   # Uncomment to destroy all objects in the bucket before destroying the bucket
-  force_destroy = true
+  # force_destroy = true
 }
 
 data "aws_iam_policy_document" "s3_bucket_policy" {
